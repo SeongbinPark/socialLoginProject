bb2a38dffe2509069836db906dd524a26a1d06b0
SeongbinPark <binni0515@gmail.com>
2024-03-17 22:28:37 +0900

    add selects

--- sql/create.sql
+++ sql/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE user(
     username VARCHAR(20),
     profileImg VARCHAR(200),
     created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
     PRIMARY KEY(userId)
 );
 
@@ -15,3 +15,20 @@ INSERT INTO user(userId, username, profileImg) VALUES
 ("3", "c", "C"),
 ("4", "d", "D"),
 ("5", "e", "E");
+
+--
+
+SELECT userId, username
+FROM user
+ORDER BY created DESC LIMIT 1;
+
+SELECT username
+FROM user
+WHERE userId="asd";
+
+DELETE FROM user
+WHERE username="박근원";
+
+UPDATE user
+SET userId="dsa"
+WHERE username="김기재";

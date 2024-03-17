b29790a7c7a7e4b5e1ca55d7bdc6e0d5f029c11f
SeongbinPark <binni0515@gmail.com>
2024-03-17 21:19:19 +0900

    insert dummy data

--- sql/create.sql
+++ sql/create.sql
@@ -6,3 +6,12 @@ CREATE TABLE user(
     updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE,
     PRIMARY KEY(userId)
 );
+
+--
+
+INSERT INTO user(userId, username, profileImg) VALUES
+("1", "a", "A"),
+("2", "b", "B"),
+("3", "c", "C"),
+("4", "d", "D"),
+("5", "e", "E");

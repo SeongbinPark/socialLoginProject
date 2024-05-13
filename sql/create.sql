7da95dcf22c7312b8d3209ea09c98a87eb2f2ef0
SeongbinPark <binni0515@gmail.com>
2024-05-13 18:48:53 +0900

    Add DB Settings

--- sql/create.sql
+++ sql/create.sql
@@ -1,21 +1,28 @@
-CREATE TABLE user(
-    userId VARCHAR(60),
-    username VARCHAR(20),
-    profileImg VARCHAR(200),
-    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    PRIMARY KEY(userId)
+DROP SCHEMA IF EXISTS socialLoginProject;
+CREATE SCHEMA IF NOT EXISTS socialLoginProject DEFAULT CHARACTER SET utf8;
+USE socialLoginProject;
+
+-- -----------------------------
+
+DROP TABLE IF EXISTS socialLoginProject.user;
+
+CREATE TABLE user (
+  userId VARCHAR(60),
+  userName VARCHAR(20),
+  profileImg VARCHAR(200),
+  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+  PRIMARY KEY(userId)
 );
 
---
+-- -----------------------------
 
-INSERT INTO user(userId, username, profileImg) VALUES
-("1", "a", "A"),
-("2", "b", "B"),
-("3", "c", "C"),
-("4", "d", "D"),
-("5", "e", "E");
-
+INSERT INTO user (userId, userName, profileImg) VALUES
+("root","근원","asdfgh"),
+("park","공원","qwerty"),
+("rlwo","기재","zxcvbn"),
+("xodn","태우","hjklop"),
+("woals","재민","tyuio");
 --
 
 SELECT userId, username
